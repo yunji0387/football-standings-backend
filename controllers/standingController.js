6eefe86899a04d938a0761ed321cb1a2fc7e3531
yunji0387 <yunji0387@gmail.com>
2024-02-07 16:08:27 -0600

    Add findStandingByCompetitionCode function to standingController.js

--- controllers/standingController.js
+++ controllers/standingController.js
@@ -22,3 +22,34 @@ exports.createStanding = async (req, res) => {
         });
     }
 };
+
+//get specific standing by code
+exports.findStandingByCompetitionCode = async (req, res) => {
+    try {
+        // Extract the code from the request. This example assumes you're passing the code as a URL parameter
+        const { code } = req.params;
+
+        // Find the standing document where the competition.code matches the code from the request
+        const standing = await Standing.findOne({ 'competition.code': code });
+
+        // If no standing is found, return a 404 Not Found response
+        if (!standing) {
+            return res.status(404).json({
+                success: false,
+                message: 'Standing not found for the provided competition code'
+            });
+        }
+
+        // If a standing is found, return it with a 200 OK response
+        res.status(200).json({
+            success: true,
+            data: standing
+        });
+    } catch (error) {
+        // If an error occurs, return a 500 Internal Server Error response
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
